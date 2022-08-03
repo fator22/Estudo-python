e0d1712f39f7bfb910a3a9372efd1be4f852aa92
fator22 <83934749+fator22@users.noreply.github.com>
2022-08-03 16:40:50 -0300

    Update desafio-02.py

--- script-python/desafio/desafio-02.py
+++ script-python/desafio/desafio-02.py
@@ -2,4 +2,3 @@ dia = input('Qual é seu dia de nacimento? ')
 mes = input('De qual mes? ')
 ano = input('Qual ano? ')
 print('Você nasceu dia',dia,'do mês',mes,'do ano de',ano,'. Esta correto?')
-            
